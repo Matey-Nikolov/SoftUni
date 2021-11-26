b6392424845d87c9d1da1f6c50f27872e4310c86
Matey <80168510+Matey-Nikolov@users.noreply.github.com>
2021-11-26 19:07:01 +0200

    Add files via upload

---
+++ C#/Basic/Matrix/Lab_Multidimensional_Arrays/Primary_Diagonal.cs
@@ -0,0 +1,34 @@
+﻿namespace Primary_Diagonal
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            int columns = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[columns, columns];
+
+            int sum = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int[] input = Console.ReadLine()
+                        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse)
+                        .ToArray();
+
+                for (int a = 0; a < input.Length; a++)
+                {
+                    matrix[i, a] = input[a];
+
+                    if (i == a)
+                        sum += matrix[i, a];
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
