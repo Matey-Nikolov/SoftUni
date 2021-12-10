6ccc4dbc80d7a5109a3d1727562d80565a806bee
Matey <80168510+Matey-Nikolov@users.noreply.github.com>
2021-12-10 10:36:25 +0200

    Add files via upload

---
+++ C#/Basic/Dictionary/Lab_Sets_and_Dictionaries/CountSameValuesinArray.cs
@@ -0,0 +1,33 @@
+﻿namespace CountSameValuesinArray
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            List<string> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Dictionary<string, int> counts = new Dictionary<string, int>();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                string currentNumber = numbers[i];
+
+                if (!counts.ContainsKey(currentNumber))
+                    counts.Add(currentNumber, 1);
+                else
+                     counts[currentNumber]++;
+            }
+
+            foreach (var num in counts)
+            {
+                Console.WriteLine($"{num.Key} - {num.Value} times");
+            }
+        }
+    }
+}
