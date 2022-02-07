4281c8db7a6ec1e759abdc55fe00f29f8c0624dd
Matey <80168510+Matey-Nikolov@users.noreply.github.com>
2022-02-07 17:37:05 +0200

    Add files via upload

---
+++ C#/Basic/Regular Expressions (RegEx)/Lab - Regular Expressions/MatchFullName.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Lab_Regular_Expressions
+{
+    class Program
+    {
+        static void Main()
+        {
+            string pattern = @"\b([A-Z][a-z]+) ([A-Z][a-z]+)\b";
+            string names = Console.ReadLine();
+
+            MatchCollection matches = Regex.Matches(names, pattern);
+
+            foreach (Match name in matches)
+            {
+                Console.WriteLine(name.Value + " ");
+            }
+
+        }
+    }
+}
