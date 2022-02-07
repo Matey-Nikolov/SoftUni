808ffbce0289475cf47b30aed70e87727f5dd752
Matey <80168510+Matey-Nikolov@users.noreply.github.com>
2022-02-07 17:36:46 +0200

    Add files via upload

---
+++ C#/Basic/Regular Expressions (RegEx)/Lab - Regular Expressions/MatchPhoneNumber.cs
@@ -0,0 +1,24 @@
+﻿namespace Match_Phone_Number_Regex
+{
+    using System;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main()
+        {
+            string pattern = @"\+359([ |-])2\1[0-9]{3}\1[0-9]{4}";
+            string phones = Console.ReadLine();
+
+            var phonsMatches = Regex.Matches(phones, pattern);
+
+            var matchedPhones = phonsMatches
+                .Cast<Match>()
+                .Select(a => a.Value.Trim())
+                .ToArray();
+
+            Console.WriteLine(string.Join(", ", matchedPhones));
+        }
+    }
+}
