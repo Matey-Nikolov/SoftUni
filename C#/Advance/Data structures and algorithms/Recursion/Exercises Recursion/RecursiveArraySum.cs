6459e0733378f048d982f4a9cd9b00b245eb0915
Matey <80168510+Matey-Nikolov@users.noreply.github.com>
2022-02-10 16:40:48 +0200

    Add files via upload

---
+++ C#/Advance/Data structures and algorithms/Recursion/Exercises Recursion/RecursiveArraySum.cs
@@ -0,0 +1,44 @@
+﻿namespace RecursiveArraySum
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            // SumTwoElements(5, 7);
+
+            int[] array = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+           // int[] array = new int[] { 1, 2, 3, 4, 5};
+            int result = SumArray(array, 0);
+            Console.WriteLine(result);
+        }
+
+        static int SumArray(int[] array, int index)
+        {
+            if (index == array.Length - 1)
+            {
+                return array[index];
+            }
+
+            int currentSum = array[index] + SumArray(array, index + 1);
+            return currentSum;
+        }
+
+        static void SumTwoElements(int a, int b)
+        {
+            int result = a + b;
+            printResult(result);
+        }
+
+        private static void printResult(int result)
+        {
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ C#/Advance/Data structures and algorithms/Recursion/Exercises Recursion/RecursiveFactorial.cs
@@ -0,0 +1,24 @@
+﻿namespace RecursionFactorial
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            long result = long.Parse(Console.ReadLine());
+            long result1 = RecursionFactorial(result);
+            Console.WriteLine(result1);
+        }
+
+        public static long RecursionFactorial(long currentNumber)
+        {
+            if (currentNumber == 0)
+            {
+                return 1;
+            }
+
+            return currentNumber * RecursionFactorial(currentNumber - 1);
+        }
+    }
+}
