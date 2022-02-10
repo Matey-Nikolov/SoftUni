ad327cfa7d6f44a048f678b99e2463f61cae9274
Matey <80168510+Matey-Nikolov@users.noreply.github.com>
2022-02-10 16:41:37 +0200

    Add files via upload

---
+++ C#/Advance/Data structures and algorithms/Recursion/Exercises Recursive Algorithms and Backtracking/Generating_0_1_Vectors.cs
@@ -0,0 +1,34 @@
+﻿namespace Generating_0_1_Vectors
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] nums = new int[n];
+
+
+            GenerateVectoRecursive(nums, 0);
+        }
+
+        // 0 0 0 0 0 0 0 0 0 - всички възмжни комбинации
+        private static void GenerateVectoRecursive(int[] nums, int index)
+        {
+            if (index > nums.Length - 1)
+            {
+                Console.WriteLine(string.Join("", nums));
+                return;
+            }
+
+            for (int i = 0; i <= 1; i++)
+            {
+                nums[index] = i;
+                GenerateVectoRecursive(nums, index + 1);
+            }
+        }
+    }
+}
