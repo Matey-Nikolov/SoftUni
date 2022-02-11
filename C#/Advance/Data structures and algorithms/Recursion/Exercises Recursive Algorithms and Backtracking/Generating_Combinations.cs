8daa407c216e5b9615bb022b89593868a2e840f0
Matey <80168510+Matey-Nikolov@users.noreply.github.com>
2022-02-11 17:32:39 +0200

    Add files via upload

---
+++ C#/Advance/Data structures and algorithms/Recursion/Exercises Recursive Algorithms and Backtracking/Generating_Combinations.cs
@@ -0,0 +1,39 @@
+﻿namespace Generating_Combinations
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            int[] set = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            
+            int k = int.Parse(Console.ReadLine());
+
+            int[] array = new int[k];
+
+            GenerateCombinations(array, 0, 0, set);
+        }
+
+        private static void GenerateCombinations(int[] array, int index, int startNum, int[] set)
+        {
+
+            if (index > array.Length - 1)
+            {
+                Console.WriteLine(string.Join(" ", array));
+                return;
+            }
+
+            // startNum == index of element from set.
+            for (int i = startNum; i < set.Length; i++)
+            {
+                array[index] = set[i];
+                GenerateCombinations(array, index + 1, i + 1, set );
+            }
+        }
+    }
+}
