ab17a4ffc941a25feb5b54f78ac756673163db1c
Matey <80168510+Matey-Nikolov@users.noreply.github.com>
2022-02-11 17:32:21 +0200

    Add files via upload

---
+++ C#/Advance/Data structures and algorithms/Recursion/Exercises Recursive Algorithms and Backtracking/Combinations_without_Repetition.cs
@@ -0,0 +1,35 @@
+﻿namespace Combinations_without_Repetition
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int k = int.Parse(Console.ReadLine());
+
+            int[] array = new int[k];
+
+            GenerateCombinations(array, 0, n, 1);
+        }
+
+
+        private static void GenerateCombinations(int[] array, int index, int n, int startNum)
+        {
+            if (index > array.Length - 1)
+            {
+                Console.WriteLine(string.Join(" ", array));
+                return;
+            }
+
+            for (int i = startNum; i <= n; i++)
+            {
+                array[index] = i;
+
+                GenerateCombinations(array, index + 1, n, i + 1); // startNum = i + 1
+            }
+        }
+    }
+}
