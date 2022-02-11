a24c22414258aa084e217308d7e6fd48e34132f5
Matey <80168510+Matey-Nikolov@users.noreply.github.com>
2022-02-11 17:32:58 +0200

    Add files via upload

---
+++ C#/Advance/Data structures and algorithms/Recursion/Exercises Recursive Algorithms and Backtracking/8_Queens_Puzzle.cs
@@ -0,0 +1,88 @@
+﻿namespace _8_Queens_Puzzle
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        const int Size = 8;
+        static int[,] matrix = new int[Size, Size];
+
+        static HashSet<int> attackedRows = new HashSet<int>();
+        static HashSet<int> attackedCols = new HashSet<int>();
+        static HashSet<int> attackedLeftDiagonals = new HashSet<int>();
+        static HashSet<int> attackedRightDiagonals = new HashSet<int>();
+
+        static void Main()
+        {
+            PlaceQueens(0);
+        }
+
+        static void PlaceQueens(int row)
+        {
+            if (row == Size)
+            {
+                PrintSolution();
+            }
+
+            for (int col = 0; col < Size; col++)
+            {
+                if (CanPlaceQueen(row, col))
+                {
+                    MarkAttackedPosition(row, col);
+                    PlaceQueens(row + 1);
+                    UnMarkAttackedPositions(row, col);
+                }
+            }
+        }
+
+        private static void UnMarkAttackedPositions(int row, int col)
+        {
+            matrix[row, col] = 0;
+            attackedRows.Remove(row);
+            attackedCols.Remove(col);
+            attackedLeftDiagonals.Remove(col - row);
+            attackedRightDiagonals.Remove(col + row);
+        }
+
+        private static void MarkAttackedPosition(int row, int col)
+        {
+            matrix[row, col] = 1;
+            attackedRows.Add(row);
+            attackedCols.Add(col);
+            attackedLeftDiagonals.Add(col - row);
+            attackedRightDiagonals.Add(col + row);
+        }
+
+        private static bool CanPlaceQueen(int row, int col)
+        {
+            bool isNotFreePosition = attackedRows.Contains(row)
+                || attackedCols.Contains(col)
+                || attackedLeftDiagonals.Contains(col - row)
+                || attackedRightDiagonals.Contains(col + row);
+
+            return !isNotFreePosition;
+        }
+
+        private static void PrintSolution()
+        {
+            for (int row = 0; row < Size; row++)
+            {
+                for (int col = 0; col < Size; col++)
+                {
+                    // TODO
+                    if (matrix[row, col] == 1)
+                    {
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write("- ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
