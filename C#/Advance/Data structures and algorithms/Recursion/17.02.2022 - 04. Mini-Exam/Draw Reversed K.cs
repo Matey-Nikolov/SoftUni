8216043f846bd732b84c50af890dcbae2625c95d
Matey-Nikolov <matey.nikolov@mail.bg>
2022-02-20 17:10:48 +0200

    Draw Reversed K

---
+++ C#/Advance/Data structures and algorithms/Recursion/17.02.2022 - 04. Mini-Exam/Draw Reversed K.cs
@@ -0,0 +1,29 @@
+﻿namespace Mini_Exam_Recursion_and_Backtracking
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            int input = int.Parse(Console.ReadLine());
+ 
+            Reversed(input, 0);
+        }
+
+        private static void Reversed(int input, int spaceCount)
+        {
+            if (input == 0)
+            {
+                return;
+            }
+
+            Console.Write(new string(' ', spaceCount));
+            Console.WriteLine(new string('#', input));
+            Reversed(input - 1, spaceCount + 1);
+            Console.Write(new string(' ', spaceCount));
+            Console.WriteLine(new string('*', input));
+
+        }
+    }
+}
