7a4563b05bb9fd78adc6eb9aa16e2465c7b97e3a
Matey-Nikolov <matey.nikolov@mail.bg>
2022-02-27 19:42:45 +0200

    Main program

---
+++ C#/Advance/Data structures and algorithms/Lab Intro to Data Structures/Linked-Strings/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Linked_Strings
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first = Console.ReadLine();
+            string last = Console.ReadLine();
+            string afterFirst = Console.ReadLine();
+            string beforeLast = Console.ReadLine();
+
+            LinkedList<string> list = new LinkedList<string>();
+            list.AddFirst(first);
+            list.AddLast(last);
+            list.AddAfter(list.First, afterFirst);
+            list.AddBefore(list.Last, beforeLast);
+
+            Console.WriteLine(String.Join(", ", list));
+
+        }
+    }
+}
