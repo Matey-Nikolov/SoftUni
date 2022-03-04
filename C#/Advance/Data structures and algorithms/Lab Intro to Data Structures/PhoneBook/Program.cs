c1f47db7ff54af02e92b44edef2cf04adf1cd024
Matey-Nikolov <matey.nikolov@mail.bg>
2022-03-04 10:32:30 +0200

    PhoneBook

---
+++ C#/Advance/Data structures and algorithms/Lab Intro to Data Structures/PhoneBook/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace PhoneBook
+{
+    using System;
+    using System.Linq;
+    using Wintellect.PowerCollections;
+
+    class Program
+    {
+        static void Main()
+        {
+            MultiDictionary<string, string> multiDic = new MultiDictionary<string, string>(true);
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] namePhone = Console.ReadLine()
+                    .Split(" - ");
+
+                string name = namePhone[0];
+                string phone = namePhone[1];
+
+                multiDic.Add(name, phone);
+            }
+
+            foreach (var kvp in multiDic.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+            }
+
+        }
+    }
+}
