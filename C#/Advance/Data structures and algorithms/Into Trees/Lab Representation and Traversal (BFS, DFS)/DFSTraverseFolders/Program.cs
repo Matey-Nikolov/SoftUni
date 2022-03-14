61f23843df840b3b3c2c68ca0e970c343eb151f5
Matey-Nikolov <matey.nikolov@mail.bg>
2022-03-14 18:23:47 +0200

    DFS Traverse Folders.

---
+++ C#/Advance/Data structures and algorithms/Into Trees/Lab Representation and Traversal (BFS, DFS)/DFSTraverseFolders/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace DFSTraverseFolders
+{
+    using System;
+    using System.IO;
+
+    class Program
+    {
+        static void Main()
+        {
+            //TraverseDirDFS(@"C:\Windows\assembly");
+        }
+
+        private static void TraverseDirDFS(DirectoryInfo dir, string spaces)
+        {
+            Console.WriteLine(spaces + dir.FullName);
+
+            DirectoryInfo[] children = dir.GetDirectories();
+
+            foreach (DirectoryInfo child in children)
+            {
+                TraverseDirDFS(child, spaces + " ");
+            }
+        }
+
+        public static void TraverseDirDFS(string directoryPath)
+        {
+            TraverseDirDFS(new DirectoryInfo(directoryPath), string.Empty);
+        }
+    }
+}
