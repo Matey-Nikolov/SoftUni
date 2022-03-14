351f3b5a5ae935031fb0a88e59469d07d724d890
Matey-Nikolov <matey.nikolov@mail.bg>
2022-03-14 18:22:20 +0200

    BFS Traverse Folders

---
+++ C#/Advance/Data structures and algorithms/Into Trees/Lab Representation and Traversal (BFS, DFS)/BFSTraverseFolders/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace BFSTraverseFolders
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    public class Program
+    {
+        static void Main()
+        {
+             TraverseDirBFS(@"C:\Windows\assembly");
+        }
+
+        public static void TraverseDirBFS(string directoryPath)
+        {
+            Queue<DirectoryInfo> visitedFolders = new Queue<DirectoryInfo>();
+
+            visitedFolders.Enqueue(new DirectoryInfo(directoryPath));
+
+            while (visitedFolders.Count > 0)
+            {
+                DirectoryInfo currentDir = visitedFolders.Dequeue();
+                Console.WriteLine(currentDir.FullName);
+
+                DirectoryInfo[] children = currentDir.GetDirectories();
+
+                foreach (DirectoryInfo child in children)
+                {
+                    visitedFolders.Enqueue(child);
+                }
+            }
+        }
+    }
+}
