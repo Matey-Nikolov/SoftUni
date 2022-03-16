23bc0b879785acf75904f8872e0876236927c119
Matey-Nikolov <matey.nikolov@mail.bg>
2022-03-16 09:43:49 +0200

    Move

--- SQL local server/SQL Language - Lab/Employee Departments.sql
+++
@@ -1,6 +0,0 @@
-SELECT TOP(5) EmployeeID, FirstName, Salary, d.Name AS 'DepartmentName'
-FROM Employees AS e
-JOIN Departments AS d
-ON d.DepartmentID = e.DepartmentID
-WHERE e.Salary >= 15000
-ORDER BY d.DepartmentID
