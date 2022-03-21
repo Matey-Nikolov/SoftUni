52f6276fe76d7137c420e68f375d8841c1ffa19f
Matey-Nikolov <matey.nikolov@mail.bg>
2022-03-21 13:40:28 +0200

    Find-File.

---
+++ C#/Advance/Data structures and algorithms/Into Trees/Exercises Trees Representation and Traversal (BFS, DFS)/Find-File/FindFile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+public static class FindFile
+{
+    private static void TraverseDirDFS(DirectoryInfo dir, string fileName)
+    {
+        try
+        {
+            FileInfo[] files = dir.GetFiles();
+
+            foreach (var direction in files)
+            {
+                if (direction.ToString() == fileName)
+                {
+                    Console.WriteLine($"{direction.Name} is found in {dir.FullName}");
+                }
+            }
+        }
+        catch
+        {
+            Console.WriteLine($"No access to {dir}");
+        }
+    }
+
+    public static void TraverseDirDFS(string directoryPath, string fileName)
+    {
+        TraverseDirDFS(new DirectoryInfo(directoryPath), fileName);
+    }
+
+    static void Main()
+    {
+        //Change directoryPath and fileName if needed
+        //TraverseDirDFS(@"C:\", "Trees-BFS-DFS-Exercises.docx"); 
+    }
+}
