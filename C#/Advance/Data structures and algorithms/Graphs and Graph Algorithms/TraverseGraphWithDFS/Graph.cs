8cd5ce6f7f10ef56d2b71464569effd5fac0f7f0
Matey-Nikolov <matey.nikolov@mail.bg>
2022-03-27 10:57:22 +0300

    Traverse Graph with DFS.

---
+++ C#/Advance/Data structures and algorithms/Graphs and Graph Algorithms/TraverseGraphWithDFS/Graph.cs
@@ -0,0 +1,54 @@
+﻿namespace TraverseGraphWithDFS
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Graph
+    {
+        private static int verticesCount;
+        private static List<int>[] adjacents;
+
+        public Graph(int verticesCountLocal)
+        {
+            adjacents = new List<int>[verticesCountLocal];
+
+            for (int i = 0; i < adjacents.Length; i++)
+            {
+                adjacents[i] = new List<int>();
+            }
+            verticesCount = verticesCountLocal;
+        }
+
+        public void AddEdge(int firstVertex, int secondVertex)
+        {
+            adjacents[firstVertex].Add(secondVertex);
+        }
+
+        public void DFS(int vertex)
+        {
+            bool[] visitedVertices = new bool[verticesCount];
+
+            DFSUtil(vertex, visitedVertices);
+        }
+
+        public void DFSUtil(int vertex, bool[] visitedVertices)
+        {
+            visitedVertices[vertex] = true;
+
+            Console.WriteLine(vertex + " ");
+
+            List<int> verticesList = adjacents[vertex];
+
+            foreach (var item in verticesList)
+            {
+                if (!visitedVertices[item])
+                {
+                    DFSUtil(item, visitedVertices);
+                }
+            }
+        }
+    }
+}
---
+++ C#/Advance/Data structures and algorithms/Graphs and Graph Algorithms/TraverseGraphWithDFS/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace TraverseGraphWithDFS
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            Graph graph = new Graph(4);
+
+            graph.AddEdge(0, 1);
+            graph.AddEdge(0, 2);
+            graph.AddEdge(1, 2);
+            graph.AddEdge(2, 0);
+            graph.AddEdge(2, 3);
+            graph.AddEdge(3, 3);
+
+            graph.DFS(2);
+        }
+    }
+}
