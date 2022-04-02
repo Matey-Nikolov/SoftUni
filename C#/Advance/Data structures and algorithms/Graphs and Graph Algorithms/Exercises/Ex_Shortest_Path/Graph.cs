4c16a1bdc0bd42f02a2d7491512a1ad951d94091
Matey-Nikolov <matey.nikolov.highschool@svetlina.net>
2022-04-02 10:28:23 +0300

    Ex_Shortest_Path.

---
+++ C#/Advance/Data structures and algorithms/Graphs and Graph Algorithms/Exercises/Ex_Shortest_Path/Graph.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex_Shortest_Path
+{
+    public class Graph
+    {
+        private static int verticesCount;
+        private static List<int>[] adjacents;
+
+        private static int countSort;
+        private static int countSortSave;
+
+        public Graph(int verticesCountLocal)
+        {
+            adjacents = new List<int>[verticesCountLocal];
+
+            for (int i = 0; i < adjacents.Length; i++)
+            {
+                adjacents[i] = new List<int>();
+            }
+            verticesCount = verticesCountLocal;
+        }
+
+        public void AddEdge(int firstVertex, int secondVertex)
+        {
+            adjacents[firstVertex].Add(secondVertex);
+            //adjacents[secondVertex].Add(firstVertex);
+        }
+
+        public void PrintSortPathCount(int startNode, int endNode)
+        {
+            bool[] isVisited = new bool[verticesCount];
+            List<int> pathList = new List<int>();
+
+            // add source to path[]
+            pathList.Add(startNode);
+
+
+
+            // Call recursive utility
+            CountPathsUtil(startNode, endNode, isVisited, pathList);
+        }
+
+        private void CountPathsUtil(int startNodeMove, int endNodeMove, bool[] isVisited, List<int> pathList)
+        {
+            if (countSortSave < 0)
+            {
+                return;
+            }
+
+            if (startNodeMove.Equals(endNodeMove))
+            {
+                if (countSortSave < countSort)
+                {
+                    countSortSave = countSort;
+                }
+                else
+                {
+                    Console.WriteLine($"Path found. Length: {countSort}");
+                }
+
+
+                countSort = 0;
+                return;
+            }
+
+            isVisited[startNodeMove] = true;
+
+
+            foreach (var itemCheck in adjacents[startNodeMove])
+            {
+                if (!isVisited[itemCheck])
+                {
+                    countSort++;
+
+                    pathList.Add(itemCheck);
+                    CountPathsUtil(itemCheck, endNodeMove, isVisited, pathList);
+                    pathList.Remove(itemCheck);
+                }
+            }
+
+            if (countSortSave == 0)
+            {
+                Console.WriteLine("No path exists");
+                countSortSave = -1;
+                return;
+            }
+
+            isVisited[startNodeMove] = false;
+
+
+        }
+    }
+}
---
+++ C#/Advance/Data structures and algorithms/Graphs and Graph Algorithms/Exercises/Ex_Shortest_Path/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Ex_Shortest_Path
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        static void Main()
+        {
+            var graph = ReadGraph();
+
+            int startVertex = int.Parse(Console.ReadLine());
+            int endVertex = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Shortest path length from {startVertex} to {endVertex}:");
+
+            graph.PrintSortPathCount(startVertex, endVertex);
+        }
+
+        public static Graph ReadGraph()
+        {
+            int verticesCount = int.Parse(Console.ReadLine());
+            Graph GraphRead = new Graph(verticesCount);
+
+            int countOfEgesN = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < countOfEgesN; i++)
+            {
+                int[] firstAndSecondVertexOfEdge = Console.ReadLine()
+                    .Split(" ")
+                    .Select(int.Parse)
+                    .ToArray();
+
+                int firstVertex = firstAndSecondVertexOfEdge[0];
+                int secondVertex = firstAndSecondVertexOfEdge[1];
+
+                GraphRead.AddEdge(firstVertex, secondVertex);
+            }
+
+            return GraphRead;
+        }
+    }
+    
+}
