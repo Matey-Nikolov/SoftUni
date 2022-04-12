12fe8c4de2c2a916d1c93acf2cf158932c6448b3
Matey-Nikolov <matey.nikolov.highschool@svetlina.net>
2022-04-12 18:49:12 +0300

    Calculator - Controllers

---
+++ Back End/Introduction to ASP.NET Core (MVC)/Exercises/CalculatorApp-Resources/CalculatorApp/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using CalculatorApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(Calculator calculator)
+        {
+            return View(calculator);
+        }
+
+
+        [HttpPost]
+        public IActionResult Calculate(Calculator calculator)
+        {
+           calculator.Result = CalculateResult(calculator);
+
+            return RedirectToAction("Index", calculator);
+        }
+
+        private decimal CalculateResult(Calculator calculator)
+        {
+            var result = 0m;
+
+            switch (calculator.Operator)
+            {
+                case "+":
+                    result = calculator.LeftOperand + calculator.RightOperand;
+                    break;
+                case "-":
+                    result = calculator.LeftOperand - calculator.RightOperand;
+                    break;
+                case "*":
+                    result = calculator.LeftOperand * calculator.RightOperand;
+                    break;
+                case "/":
+                    result = calculator.LeftOperand / calculator.RightOperand;
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
