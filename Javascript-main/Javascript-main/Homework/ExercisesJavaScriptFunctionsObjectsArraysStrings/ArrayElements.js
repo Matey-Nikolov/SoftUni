39bc8fabfde5d5eafc3f2c0b894169856b137efc
Matey-Nikolov <matey.nikolov.highschool@svetlina.net>
2022-04-18 16:11:41 +0300

    modified

--- Javascript-main/Javascript-main/Homework/ExercisesJavaScriptFunctionsObjectsArraysStrings/ArrayElements.js
+++
@@ -1,9 +0,0 @@
-printElements([1, 2, 3, 4, 5])
-
-function printElements(input){
-    let element = input;
-
-    for(let i = 0; i < element.length; i++){
-        console.log(element[i]);
-    }
-}
--- Javascript-main/Javascript-main/Homework/ExercisesJavaScriptFunctionsObjectsArraysStrings/MultiplyTwoNumbers.js
+++
@@ -1,8 +0,0 @@
-let xy = [1, 2];
-
-Multiplies(xy);
-
-function Multiplies(xy){
-    let multi = xy[0] * xy[1];
-    console.log(multi);
-}
