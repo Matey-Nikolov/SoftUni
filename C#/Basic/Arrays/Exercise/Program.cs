7ccd13c418eb44324b0183ce074223125f59c207
Matey-Nikolov <matey.nikolov@mail.bg>
2022-04-28 11:11:54 +0300

    Program

---
+++ C#/Basic/Arrays/Exercise/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace CompanyUsers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            string[] companyNameEmployeeId = Console.ReadLine()
+                    .Split(" ")
+                    .ToArray();
+
+            Dictionary<string, List<string>> companyUsers = new Dictionary<string, List<string>>();
+
+            while (companyNameEmployeeId[0] != "End")
+            {
+                string company = companyNameEmployeeId[0];
+                string employeeId = companyNameEmployeeId[2];
+
+                if (!companyUsers.ContainsKey(company))
+                {
+                    companyUsers.Add(company, new List<string>() { employeeId});
+                }
+                else if (!companyUsers[company].Contains(employeeId))
+                {
+                    companyUsers[company].Add(employeeId);
+                }
+
+                companyNameEmployeeId = Console.ReadLine()
+                    .Split(" ")
+                    .ToArray();
+            }
+
+            courseNameRegisteredStudents = courseNameRegisteredStudents
+     .OrderByDescending(x => x.Value.Count())
+     .ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var item in companyUsers.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{item.Key}");
+
+                foreach (var item2 in item.Value)
+                {
+
+                    Console.WriteLine($"-- {item2}");
+                }
+            }
+        }
+    }
+}
