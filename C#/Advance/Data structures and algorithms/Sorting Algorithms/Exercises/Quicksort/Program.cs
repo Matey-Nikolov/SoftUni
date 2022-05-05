9d95e09e693f07fc49de8a73d2871fb04f3bb047
Matey-Nikolov <matey.nikolov@mail.bg>
2022-05-05 11:21:59 +0300

    Quick program

---
+++ C#/Advance/Data structures and algorithms/Sorting Algorithms/Exercises/Quicksort/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Quicksort
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        static void Main()
+        {
+            // https://www.programiz.com/dsa/quick-sort
+            Quick quickSort = new Quick();
+
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int size = numbers.Length;
+
+            quickSort.QuickSort(numbers, 0, size - 1);
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
