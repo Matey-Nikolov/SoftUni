4acd012a815f015de940c6a1449fc37bd6c52781
Matey-Nikolov <matey.nikolov@mail.bg>
2022-05-05 11:23:29 +0300

    Merge sort - program

---
+++ C#/Advance/Data structures and algorithms/Sorting Algorithms/Exercises/MergeSort/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace MergeSort
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            MergeSortClass.Sort(numbers);
+
+            Console.WriteLine(String.Join(" ", numbers));
+        }
+    }
+}
