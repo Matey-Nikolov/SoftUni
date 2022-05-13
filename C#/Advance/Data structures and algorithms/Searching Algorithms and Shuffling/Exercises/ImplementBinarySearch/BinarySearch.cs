b30c9ae4783f1dc9f41c8356f0c128c68c86f7be
Matey-Nikolov <matey.nikolov@mail.bg>
2022-05-13 08:31:46 +0300

    Class - BinarySearch

---
+++ C#/Advance/Data structures and algorithms/Searching Algorithms and Shuffling/Exercises/ImplementBinarySearch/BinarySearch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplementBinarySearch
+{
+    public class BinarySearch
+    {
+        public int IndexOf(int[] arrayNumber, int key)
+        {
+            int leftIndex = 0;
+            int rightIndex = arrayNumber.Length - 1;
+
+            while (leftIndex <= rightIndex)
+            {
+                int midlleIndex = (leftIndex + rightIndex) / +2;
+                if (key < arrayNumber[midlleIndex])
+                {
+                    rightIndex = midlleIndex - 1;
+                }
+                else if (key > arrayNumber[midlleIndex])
+                {
+                    leftIndex = midlleIndex + 1;
+                }
+                else
+                {
+                    return midlleIndex;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
