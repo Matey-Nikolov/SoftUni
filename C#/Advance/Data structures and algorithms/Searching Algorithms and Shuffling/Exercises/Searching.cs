9cc0c9e483da39ece011ff1c2b97a023c8461cd6
Matey-Nikolov <matey.nikolov@mail.bg>
2022-05-13 08:31:18 +0300

    Searching

---
+++ C#/Advance/Data structures and algorithms/Searching Algorithms and Shuffling/Exercises/Searching.cs
@@ -0,0 +1,34 @@
+﻿namespace Searching
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            // Linear Search
+
+            int[] arrayNumber = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int givenNumber = int.Parse(Console.ReadLine());
+
+            int indexSave = -1;
+
+            for (int i = 0; i < arrayNumber.Length; i++)
+            {
+                if (givenNumber == arrayNumber[i])
+                {
+                    indexSave = i;
+                    Console.WriteLine(indexSave);
+                    return;
+                }
+            }
+
+            Console.WriteLine(indexSave);
+        }
+    }
+}
