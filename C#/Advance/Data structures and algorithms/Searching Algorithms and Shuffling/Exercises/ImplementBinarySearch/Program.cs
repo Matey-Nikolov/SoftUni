ff2ec3ff7d990d5178948c72b3db632d83141008
Matey-Nikolov <matey.nikolov@mail.bg>
2022-05-13 08:31:59 +0300

    .

---
+++ C#/Advance/Data structures and algorithms/Searching Algorithms and Shuffling/Exercises/ImplementBinarySearch/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ImplementBinarySearch
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int numberFind = int.Parse(Console.ReadLine());
+
+            BinarySearch search = new BinarySearch();
+            int findYesNo = search.IndexOf(numbers, numberFind);
+
+            Console.WriteLine(findYesNo);
+        }
+    }
+}
