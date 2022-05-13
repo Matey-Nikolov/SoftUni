a7e41a34385b0fad773ca7e9752b45ede0a7eede
Matey-Nikolov <matey.nikolov@mail.bg>
2022-05-13 08:31:00 +0300

    ShuffleWords

---
+++ C#/Advance/Data structures and algorithms/Searching Algorithms and Shuffling/Exercises/ShuffleWords.cs
@@ -0,0 +1,26 @@
+﻿namespace ShuffleWords
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            string[] input = Console.ReadLine()
+                 .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Random randomIndex = new Random();
+
+            for (int i = input.Length - 1; i >= 0 ; i--)
+            {
+                int shuffleIndex = randomIndex.Next(i + 1);
+                string temp = input[i];
+                input[i] = input[shuffleIndex];
+                input[shuffleIndex] = temp;
+            
+                Console.WriteLine(input[i]);
+            }
+
+        }
+    }
+}
