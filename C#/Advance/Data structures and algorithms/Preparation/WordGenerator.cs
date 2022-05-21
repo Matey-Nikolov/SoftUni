36ccef0e6e81fa2ee4e1ea989b15e45fc0df6d8b
Matey-Nikolov <matey.nikolov@mail.bg>
2022-05-21 10:49:10 +0300

    WordGenerator

---
+++ C#/Advance/Data structures and algorithms/Preparation/WordGenerator.cs
@@ -0,0 +1,37 @@
+﻿namespace WordGenerator
+{
+    using System;
+
+    class Program
+    {
+        static char[] array;
+
+        static void Main()
+        {
+            int inputN = int.Parse(Console.ReadLine());
+            array = new char[inputN];
+
+            if (inputN > 26)
+            {
+                inputN = 26;
+            }
+
+            NestedLoopsWordGenerator(inputN, 0);
+        }
+
+        public static void NestedLoopsWordGenerator(int inputN, int current)
+        {
+            if (current >= inputN)
+            {
+                Console.WriteLine(string.Join("", array));
+                return;
+            }
+
+            for (int i = 97; i < 97 + inputN; i++)
+            {
+                array[current] = (char)i;
+                NestedLoopsWordGenerator(inputN, current + 1);
+            }
+        }
+    }
+}
