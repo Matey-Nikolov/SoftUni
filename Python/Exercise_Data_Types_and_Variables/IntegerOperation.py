d51dfe1c1f7972c966212ed6fc9b893a3cc54a30
Matey-Nikolov <matey.nikolov@mail.bg>
2022-06-19 12:34:20 +0300

    IntegerOperation

---
+++ Python/Exercise_Data_Types_and_Variables/IntegerOperation.py
@@ -0,0 +1,7 @@
+numberOne = int(input())
+numberTwo = int(input())
+numberThree = int(input())
+numberFour  = int(input())
+
+result = ((numberOne + numberTwo) / numberThree) * numberFour
+print(f'{result:.0f}')
