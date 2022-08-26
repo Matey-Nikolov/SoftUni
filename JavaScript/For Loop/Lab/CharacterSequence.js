efac3b17826bc23437af803efbdb537925868593
Matey-Nikolov <matey.nikolov@mail.bg>
2022-08-26 12:32:14 +0300

    CharacterSequence.js

---
+++ JavaScript/For Loop/Lab/CharacterSequence.js
@@ -0,0 +1,10 @@
+function CharacterSequence(input) {
+    for (let i = 0; i < input.length; i++) {
+        let letter = input[i]
+
+        console.log(letter)
+    }
+
+}
+
+CharacterSequence("softuni")
