ea5309f12c705114ddcafea32fad7782bef0f99c
Matey-Nikolov <matey.nikolov@mail.bg>
2022-10-06 13:04:36 +0300

    Introduction to JavaScript - Lab

--- JavaScript/Introduction to JavaScript - exercise/VacationBooksList.js
+++
@@ -1,9 +0,0 @@
-function booksForSummer(pages, pagesPerHour, days) {
-    let totalReading = pages / pagesPerHour
-    let requiredHours = totalReading / days
-
-    console.log(requiredHours);
-}
-
-booksForSummer(212, 20, 2)
-booksForSummer(432, 15, 4)
