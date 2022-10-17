debc2d76d0c3ca093cedbce5f4c41dd683832392
Matey-Nikolov <matey.nikolov@mail.bg>
2022-10-17 08:53:28 +0300

    convertMetersToKilometers.js

---
+++ JavaScript/Data Types and Variables/Exercises/convertMetersToKilometers.js
@@ -0,0 +1,7 @@
+function convertMetersToKilometers(meters) {
+    let convert = meters / 1000;
+    console.log(convert.toFixed(2));
+}
+
+convertMetersToKilometers(1852);
+convertMetersToKilometers(798);
