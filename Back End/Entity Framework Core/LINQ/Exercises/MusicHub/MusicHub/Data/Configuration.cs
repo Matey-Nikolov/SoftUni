8353a8993e2741c71f6f6a95140cef419ad2d9d5
Matey-Nikolov <matey.nikolov@mail.bg>
2022-10-20 09:04:46 +0300

    Configuration and MusicHubDbContext

---
+++ Back End/Entity Framework Core/LINQ/Exercises/MusicHub/MusicHub/Data/Configuration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicHub.Data
+{
+    internal class Configuration
+    {
+        public static string ConnectionString = "\"Server=(localdb)\\MSSQLLocalDB;Database=MusicHub;Integrated Security=True;\" -Provider Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models";
+    }
+}
---
+++ Back End/Entity Framework Core/LINQ/Exercises/MusicHub/MusicHub/Data/MusicHubDbContext.cs
@@ -0,0 +1,28 @@
+﻿namespace MusicHub.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using MusicHub.Data.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    internal class MusicHubDbContext : DbContext
+    {
+        public MusicHubDbContext()
+        {
+        }
+
+        public MusicHubDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Performer> Performers { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<SongPerformer> SongPerformers { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+
+    }
+}
