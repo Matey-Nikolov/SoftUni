8ca648c3f5e20fbff7df40aa5bc11d45000f9c25
Matey-Nikolov <matey.nikolov@mail.bg>
2022-10-20 09:05:02 +0300

    All

---
+++ Back End/Entity Framework Core/LINQ/Exercises/MusicHub/MusicHub/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace MusicHub
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using System.Globalization;
+    using Microsoft.VisualBasic;
+    using System.Numerics;
+    using MusicHub.Data;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            MusicHubDbContext context = new MusicHubDbContext();
+
+            // 2
+            Console.WriteLine(ExportAlbumsInfo(context, 9));
+            // 3
+            Console.WriteLine(ExportSongsAboveDuration(context, 4));
+        }
+
+        public static string ExportAlbumsInfo(MusicHubDbContext context, int producerId)
+        {
+
+            return "";
+        }
+
+        public string ExportSongsAboveDuration(MusicHubDbContext context, int duration)
+        {
+            return "";
+        }
+    }
+}
