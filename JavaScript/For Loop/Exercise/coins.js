0c8ce3dc90ff2c07634cb101043bf3fd100b71bd
Matey-Nikolov <matey.nikolov@mail.bg>
2022-10-29 19:24:06 +0300

    coins.js

---
+++ JavaScript/For Loop/Exercise/coins.js
@@ -0,0 +1,32 @@
+function coins(input) {
+    let amount = Math.round(input * 100);
+
+    let coinsCount = 0;
+    while (amount > 0) {
+        if (amount >= 200) {
+            amount -= 200;
+        } else if (amount >= 100) {
+            amount -= 100;
+        } else if (amount >= 50) {
+            amount -= 50;
+        } else if (amount >= 20) {
+            amount -= 20;
+        } else if (amount >= 10) {
+            amount -= 10
+        } else if (amount >= 5) {
+            amount -= 5;
+        } else if (amount >= 2) {
+            amount -= 2;
+        } else if (amount >= 1) {
+            amount -= 1;
+        }
+        coinsCount++;
+    }
+
+    console.log(coinsCount);
+}
+
+coins(1.23);
+coins(2);
+coins(0.56);
+coins(2.73);
