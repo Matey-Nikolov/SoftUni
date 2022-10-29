87057580e1efb9a9bc389451322902df63fb2b07
Matey-Nikolov <matey.nikolov@mail.bg>
2022-10-29 19:23:28 +0300

    leapYears.js

---
+++ JavaScript/For Loop/Exercise/leapYears.js
@@ -0,0 +1,9 @@
+function leapYears(input, randomInput) {
+    for (let i = input; i <= randomInput; i += 4) {
+        console.log(i);
+    }
+}
+
+leapYears(1908, 1919);
+leapYears(2000, 2011);
+leapYears(1584, 1597);
