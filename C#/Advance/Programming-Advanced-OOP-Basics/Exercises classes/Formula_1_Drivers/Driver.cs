ad3964fe674c76a6876c62eef21198de22808220
Matey-Nikolov <matey.nikolov@mail.bg>
2023-02-02 18:54:53 +0200

    Formula_1_Drivers

---
+++ C#/Advance/Programming-Advanced-OOP-Basics/Exercises classes/Formula_1_Drivers/Driver.cs
@@ -0,0 +1,21 @@
+﻿namespace Formula_1_Drivers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public class Driver
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public float TotalTime { get; set; }
+        public float Speed { get; set; }
+
+        public Driver(string name, int age, float totalTime, float speed)
+        {
+            Name = name;
+            Age = age;
+            TotalTime = totalTime;
+            Speed = speed;
+        }
+    }
+}
