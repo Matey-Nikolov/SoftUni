cd08279abbfca169fe8b25c40c78558ea0a79b70
Matey-Nikolov <matey.nikolov@mail.bg>
2023-02-05 15:40:15 +0200

    Antique Book store

---
+++ C#/Advance/Programming-Advanced-OOP-Basics/Exercises classes/AntiqueBookstore/Book.cs
@@ -0,0 +1,46 @@
+﻿namespace AntiqueBookstore
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PubDate  { get; set; }
+        public decimal Price { get; set; }
+        public string Publisher { get; set; }
+        public string Discount { get; set; }
+
+        public Book(string title, string author, int pubDate, decimal price)
+        {
+            Title = title;
+            Author = author;
+            PubDate = pubDate;
+            Price = price;
+            Publisher = "Unknown";
+            Discount = "Unavailable";
+        }
+        public Book(string title, string author, int pubDate, decimal price, string publisher)
+        {
+            Title = title;
+            Author = author;
+            PubDate = pubDate;
+            Price = price;
+            Publisher = publisher;
+            Discount = "Unavailable";
+        }
+
+        public Book(string title, string author, int pubDate, decimal price, string publisher, string discount)
+        {
+            Title = title;
+            Author = author;
+            PubDate = pubDate;
+            Price = price;
+            Publisher = publisher;
+            Discount = discount;
+        }
+    }
+}
