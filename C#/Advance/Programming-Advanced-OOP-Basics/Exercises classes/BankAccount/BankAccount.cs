3caee9fef674e71832111b364f5426308884fa0f
Matey-Nikolov <matey.nikolov@mail.bg>
2023-02-05 15:40:41 +0200

    Bank account

---
+++ C#/Advance/Programming-Advanced-OOP-Basics/Exercises classes/BankAccount/BankAccount.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    class BankAccount
+    {
+        public string AccountNumber { get; set; }
+        public string OwnerName { get; set; }
+
+        public decimal AccountBalance { get; set; }
+
+        public BankAccount(string accountNumber, string ownerName, decimal accountBalance)
+        {
+            AccountNumber = accountNumber;
+            OwnerName = ownerName;
+            AccountBalance = accountBalance;
+        }
+
+        public void MakeDeposit(decimal amount)
+        {
+            AccountBalance += amount;
+            Console.WriteLine($"Account balance: { AccountBalance }");
+        }
+
+        public void MakeWithdrawal(decimal draw)
+        {
+            if(draw > AccountBalance)
+            {
+                Console.WriteLine("Non-Sufficient Funds");
+            }
+            else
+            {
+                AccountBalance -= draw;
+                Console.WriteLine($"Withdrawn funds: { draw}. Funds available on the account: { AccountBalance }");
+            }
+        }
+
+
+    }
+}
---
+++ C#/Advance/Programming-Advanced-OOP-Basics/Exercises classes/BankAccount/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BankAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ");
+
+            string accNumber = input[0];
+            string owner = input[1] + " " + input[2];
+            decimal balance = decimal.Parse(input[3]);
+
+
+            var bankacc = new BankAccount(accNumber, owner, balance);
+
+            string[] n = Console.ReadLine().Split(" ");
+
+            while(n[0] != "End")
+            {
+                if(n[0] == "Deposit")
+                {
+                    bankacc.MakeDeposit(decimal.Parse(n[1]));
+                }
+                else if (n[0] == "Withdrawal")
+                {
+                    bankacc.MakeWithdrawal(decimal.Parse(n[1]));
+                }
+                n = Console.ReadLine().Split(" ");
+            }
+        }
+    }
+}
