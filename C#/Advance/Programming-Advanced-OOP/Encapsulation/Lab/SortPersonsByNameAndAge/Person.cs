0c9f783c63563af258667456931a7c23e8888a07
Matey-Nikolov <matey.nikolov@mail.bg>
2023-02-19 19:05:35 +0200

    update

---
+++ C#/Advance/Programming-Advanced-OOP/Encapsulation/Lab/SortPersonsByNameAndAge/Person.cs
@@ -0,0 +1,118 @@
+﻿namespace PersonsInfo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    public class Person
+    {
+        private int age;
+        private string firstName;
+        private string lastName;
+        private decimal salary;
+
+        public int Age
+        {
+            get { return age; }
+            private set
+            {
+                if (value <= 0 )
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+
+                age = value;
+            }
+        }
+
+        public string FirstName
+        {
+            get { return firstName; }
+            private set 
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+
+                firstName = value; 
+            }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            private set 
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+
+                lastName = value; 
+            }
+        }
+
+        public decimal Salary
+        {
+            get { return salary; }
+            private set 
+            {
+                if (value < 460)
+                {
+                    throw new ArgumentException("Salary cannot be less than 460 leva!");
+                }
+
+                salary = value; 
+            }
+        }
+
+        /*
+         * Sort Persons by Name and Age
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+        */
+
+        //Salary Increase
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (Age > 30)
+            {
+                Salary += Salary * percentage / 100;
+            }
+            else
+            {
+                Salary += Salary * percentage / 200;
+            }
+        }
+
+        /*
+         * Sort Persons by Name and Age
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} is {Age} years old.";
+        }
+        */
+
+        /*
+        //Salary Increase
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:f2} leva.";
+        }
+        */
+    }
+}
---
+++ C#/Advance/Programming-Advanced-OOP/Encapsulation/Lab/SortPersonsByNameAndAge/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace PersonsInfo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            // Sort Persons by Name and Age
+            /*
+            int lines = int.Parse(Console.ReadLine());
+            List<Person> persons = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split();
+                var person = new Person(cmdArgs[0], cmdArgs[1], int.Parse(cmdArgs[2]));
+                persons.Add(person);
+            }
+
+            persons.OrderBy(p => p.FirstName)
+                .ThenBy(p => p.Age)
+                .ToList()
+                .ForEach(p => Console.WriteLine(p.ToString()));
+
+            */
+
+            
+            // Salary Increase
+            int lines = int.Parse(Console.ReadLine());
+            List<Person> persons = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split();
+                var person = new Person(
+                    cmdArgs[0], 
+                    cmdArgs[1], 
+                    int.Parse(cmdArgs[2]), 
+                    decimal.Parse(cmdArgs[3]));
+
+                persons.Add(person);
+            }
+
+            /*
+            var parcentage = decimal.Parse(Console.ReadLine());
+            persons.ForEach(p => p.IncreaseSalary(parcentage));
+            persons.ForEach(p => Console.WriteLine(p.ToString()));
+            */
+
+            //First and Reserve Team
+            Team team = new Team("SoftUni");
+
+            foreach (var person in persons)
+            {
+                team.AddPlayer(person);
+            }
+
+            Console.WriteLine(team.ToString());
+        }
+    }
+}
