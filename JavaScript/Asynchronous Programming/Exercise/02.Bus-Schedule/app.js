8373ce80f261a54b151c8bb018cd92dacc50f104
Matey-Nikolov <matey.nikolov@mail.bg>
2023-02-27 18:34:38 +0200

    02

---
+++ JavaScript/Asynchronous Programming/Exercise/02.Bus-Schedule/app.js
@@ -0,0 +1,45 @@
+function solve() {
+
+    let getInfo = document.getElementById('info');
+    let buttonDepart = document.getElementById('depart');
+    let buttonArrive = document.getElementById('arrive');
+
+   async function getData(){
+        const stops = await fetch(`http://localhost:3030/jsonstore/bus/schedule/depot`) // schedule/serdtfdepot
+        .then(response => response.json())
+        .catch(err => 
+            {
+                getInfo.textContent = 'Error';
+                buttonDepart.disabled = true;
+                buttonArrive.disabled = true;
+            });
+
+        return stops;
+    }
+
+    let nextStop;
+
+    async function depart() {
+        buttonDepart.disabled = true;
+        buttonArrive.disabled = false;
+
+        nextStop = await getData();
+
+        console.log(nextStop);
+        getInfo.textContent = `Next stop ${nextStop.name}`;
+    }
+
+    function arrive() {
+        buttonDepart.disabled = false;
+        buttonArrive.disabled = true;
+        
+        getInfo.textContent = `Arriving at ${nextStop.name}`;
+    }
+
+    return {
+        depart,
+        arrive
+    };
+}
+
+let result = solve();
