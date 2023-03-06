0b74b7bb6d222184b0df57f1c63d77611c335f54
Matey-Nikolov <matey.nikolov@mail.bg>
2023-03-06 19:58:30 +0200

    update

--- JavaScript/Asynchronous Programming/Exercise/04.Locked-Profile/app.js
+++ JavaScript/Asynchronous Programming/Exercise/04.Locked-Profile/app.js
@@ -4,50 +4,48 @@ async function lockedProfile() {
 	const dataJSON = await data.json()
     
     const main = document.getElementById('main');
-    main.innerHTML = '';
-
-    const div = document.createElement('div');
-    const btn = document.createElement('button');
+    //main.innerHTML = '';
 
     for (const key in dataJSON)
-    {
+    {   
+        const div = document.createElement('div');
+        const btn = document.createElement('button');
+
         btn.textContent = 'Show more';
         div.className = 'profile';
         div.innerHTML = `
-        <img src="./iconProfile2.png" class="userIcon"> />
+        <img src="./iconProfile2.png" class="userIcon">
         <label>Lock</label>
-        <input type="radio" name="user${dataJSON[key].id}Locked" value="lock" checked>
+        <input type="radio" name="user${dataJSON[key]._id}Locked" value="lock" checked="">
         <label>Unlock</label>
-        <input type="radio" name="user${dataJSON[key].id}Locked" value="unlock"><br>
+        <input type="radio" name="user${dataJSON[key]._id}Locked" value="unlock"><br>
         <hr>
         <label>Username</label>
-        <input type="text" name="user${dataJSON[key].id}Username" value=${dataJSON[key].username} disabled readonly />
-        <div id="user2HiddenFields">
-            <hr>
-            <label>Email:</label>
-            <input type="email" name="user${dataJSON[key].id}Email" value=${dataJSON[key].email} disabled readonly />
-            <label>Age:</label>
-            <input type="email" name="user${dataJSON[key].id}Age" value=${dataJSON[key].age} disabled readonly />
-        </div>
-        <button>Show more</button>`;
+        <input type="text" name="user${dataJSON[key]._id}Username" value=${dataJSON[key].username} disabled="" readonly="">
+        <div id="user${dataJSON[key]._id}HiddenFields">
+        <hr>
+        <label>Email:</label>
+        <input type="email" name="user${dataJSON[key]._id}Email" value=${dataJSON[key].email} disabled="" readonly="">
+        <label>Age:</label>
+        <input type="email" name="user${dataJSON[key]._id}Age" value=${dataJSON[key].age} disabled="" readonly="">
+        </div>`;
 
         btn.addEventListener('click', () => {
             const checked = div.querySelector('input[type=radio]:checked')
             if (checked && checked.value === 'unlock') {
                 if (btn.innerText === 'Show more') {
-                    div.querySelector(`#user${dataJSON[key].id}HiddenFields`).style.display = 'block'
-                    btn.innerText = 'Hide it'
+                    div.querySelector(`#user${dataJSON[key]._id}HiddenFields`).style.display = 'block'
+                    btn.innerText = 'Hide it';
                 } else {
-                    div.querySelector(`#user${dataJSON[key].id}HiddenFields`).style.display = 'none'
-                    btn.innerText = 'Show more'
+                    div.querySelector(`#user${dataJSON[key]._id}HiddenFields`).style.display = 'none'
+                    btn.innerText = 'Show more';
                 }
             }
         })
 
         div.appendChild(btn);
+
         main.appendChild(div);
+        console.log(main);
     }
-
-    
-
 }
