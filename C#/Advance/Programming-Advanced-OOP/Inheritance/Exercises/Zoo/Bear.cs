a70be92de6cf725bf8d99b27604e89a4cce7e5be
Matey-Nikolov <matey.nikolov@mail.bg>
2023-03-24 19:02:39 +0200

    class

---
+++ C#/Advance/Programming-Advanced-OOP/Inheritance/Exercises/Zoo/Bear.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo
+{
+    public class Bear : Mammal
+    {
+        public string Name { get; set; }
+        public Bear(string name) : base(name)
+        { 
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"Bear's name: {Name}";
+        }
+    }
+}
