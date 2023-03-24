08b84302ca613c835338487ebe3a1ef861122d53
Matey-Nikolov <matey.nikolov@mail.bg>
2023-03-24 19:02:50 +0200

    class

---
+++ C#/Advance/Programming-Advanced-OOP/Inheritance/Exercises/Zoo/Lizard.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Zoo
+{
+    public class Lizard : Reptile
+    {
+        public string NameLizard { get; set; }
+        
+        public Lizard(string name) : base(name)
+        { 
+            NameLizard = name;
+        }
+
+        public override string ToString()
+        {
+            return $"Lizard's name: {NameLizard}";
+        }
+    }
+}
---
+++ C#/Advance/Programming-Advanced-OOP/Inheritance/Exercises/Zoo/Reptile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo
+{
+    public class Reptile : Animal
+    {
+        public string NameReptile { get; set; }
+
+        public Reptile(string name)
+        {
+            NameReptile = name;
+        }
+
+        public override string ToString()
+        {
+            return $"Reptile's name: {NameReptile}";
+        }
+    }
+}
