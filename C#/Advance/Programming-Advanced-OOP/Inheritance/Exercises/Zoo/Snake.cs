6302e9a152e07fad6561ff0e05f99d8baf1b009a
Matey-Nikolov <matey.nikolov@mail.bg>
2023-03-24 19:02:59 +0200

    class

---
+++ C#/Advance/Programming-Advanced-OOP/Inheritance/Exercises/Zoo/Snake.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Zoo
+{
+    public class Snake : Reptile
+    {
+        public string NameSnake { get; set; }
+
+        public Snake(string name) : base(name)
+        {
+            NameSnake = name;
+        }
+
+        public override string ToString()
+        {
+            return $"Snake's name: {NameSnake}";
+        }
+    }
+}
---
+++ C#/Advance/Programming-Advanced-OOP/Inheritance/Exercises/Zoo/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Zoo
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string gorillaName = Console.ReadLine();
+            Gorilla gorilla = new Gorilla(gorillaName);
+
+            string snakeNname = Console.ReadLine();
+            Snake snake = new Snake(snakeNname);
+
+            string lizardName = Console.ReadLine();
+            Lizard lizard = new Lizard(lizardName);
+
+            string bearName = Console.ReadLine();
+            Bear bear = new Bear(bearName);
+
+            Console.WriteLine(gorilla.ToString());
+            Console.WriteLine(snake.ToString());
+            Console.WriteLine(lizard.ToString());
+            Console.WriteLine(bear.ToString());
+        }
+    }
+}
