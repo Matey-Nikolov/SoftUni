7fde1b4e1687cc6d61178de87de147d2be250335
Matey-Nikolov <matey.nikolov@mail.bg>
2023-04-01 18:55:44 +0300

    Define_Interface_IPerson

---
+++ C#/Advance/Programming-Advanced-OOP/Interfaces and Abstraction/Define_Interface_IPerson/Citizen.cs
@@ -0,0 +1,23 @@
+﻿namespace PersonInfo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Citizen : IPerson, IIdentifiable, IBirthable
+    {
+        public string Id { get; set; }
+
+        public string Birthdate { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Citizen(string name, int age, string id, string birthdate)
+        {
+            Id = id;
+            Birthdate = birthdate;
+            Name = name;
+            Age = age;
+        }
+    }
+}
