ee289e9c1474d2558a8f28557e68d01b2bc9a8dc
Matey-Nikolov <matey.nikolov@mail.bg>
2023-04-01 18:55:11 +0300

    Explicit_Interfaces

---
+++ C#/Advance/Programming-Advanced-OOP/Interfaces and Abstraction/Explicit_Interfaces/Citizen.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Explicit_Interfaces
+{
+    public class Citizen : IPerson, IResident
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+
+        public int Age { get; set; }
+
+        public Citizen(string name, string country, int age)
+        {
+            Name = name;
+            Country = country;
+            Age = age;
+        }
+        string IPerson.GetName()
+        {
+            return "Mr/Ms/Mrs";
+        }
+
+        string IResident.GetName()
+        {
+            return Name;
+        }
+    }
+}
---
+++ C#/Advance/Programming-Advanced-OOP/Interfaces and Abstraction/Explicit_Interfaces/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Explicit_Interfaces
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (input[0] != "End")
+            {
+                string name = input[0];
+                string country = input[1];
+                int age = int.Parse(input[2]);
+
+                IPerson citizen = new Citizen(name, country, age);
+                IResident citizen1 = new Citizen(name, country, age);
+
+                Console.WriteLine(citizen.Name);
+                Console.WriteLine($"{citizen.GetName()} {citizen1.GetName()}");
+
+                input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+        }
+    }
+}
