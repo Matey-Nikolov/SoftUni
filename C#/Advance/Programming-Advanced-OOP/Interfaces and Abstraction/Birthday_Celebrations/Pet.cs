5071440d48252bcbf99777d39b1bb6ca2bfa396b
Matey-Nikolov <matey.nikolov@mail.bg>
2023-04-02 18:56:17 +0300

    Birthday_Celebrations

---
+++ C#/Advance/Programming-Advanced-OOP/Interfaces and Abstraction/Birthday_Celebrations/Pet.cs
@@ -0,0 +1,17 @@
+﻿namespace Birthday_Celebrations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Pet : IBirthable
+    {
+        public string Name { get; set; }
+        public DateTime Birthdate { get; set; }
+        public Pet(string name, string birthdate)
+        {
+            Name = name;
+            Birthdate = DateTime.ParseExact(birthdate, "dd/mm/yyyy", null);
+        }
+    }
+}
---
+++ C#/Advance/Programming-Advanced-OOP/Interfaces and Abstraction/Birthday_Celebrations/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Birthday_Celebrations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Numerics;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IBirthable> birthables = new List<IBirthable>();
+
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string nameOrModel;
+            int age;
+            BigInteger id;
+            string birthdate;
+
+            while (input[0] != "End")
+            {
+                switch(input[0]) 
+                {
+                    case "Citizen":
+                        nameOrModel = input[1];
+                        age = int.Parse(input[2]);
+                        id = BigInteger.Parse(input[3]);
+                        birthdate = input[4];
+
+                        Citizen citizen = new Citizen(id, nameOrModel, age, birthdate);
+                        birthables.Add(citizen);
+                        break;
+                    case "Pet":
+                        nameOrModel = input[1];
+                        birthdate = input[2];
+
+                        Pet pet = new Pet(nameOrModel, birthdate);
+                        birthables.Add(pet);
+                        break;
+                    case "Robot":
+                        Robot robot = new Robot();
+                        break;
+                }
+
+                input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            int year = int.Parse(Console.ReadLine());
+
+            birthables = birthables.Where(x => x.Birthdate.Year == year)
+                .ToList();
+
+            foreach (var item in birthables)
+            {
+                Console.WriteLine($"{item.Birthdate:dd/mm/yyyy}");
+            }
+        }
+    }
+}
