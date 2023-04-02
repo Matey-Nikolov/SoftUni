7fdb96455cd67106bb20433ae4002ebe61971720
Matey-Nikolov <matey.nikolov@mail.bg>
2023-04-02 18:56:30 +0300

    Birthday_Celebrations

---
+++ C#/Advance/Programming-Advanced-OOP/Interfaces and Abstraction/Birthday_Celebrations/Citizen.cs
@@ -0,0 +1,24 @@
+﻿namespace Birthday_Celebrations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Numerics;
+    using System.Text;
+
+    public class Citizen : IBirthable, IIdentifiable
+    {
+        public BigInteger Id { get; set; }
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+        public DateTime Birthdate { get; set; }
+
+        public Citizen(BigInteger id, string name, int age, string birthdate)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            Birthdate = DateTime.ParseExact(birthdate, "dd/mm/yyyy", null);
+        }
+    }
+}
