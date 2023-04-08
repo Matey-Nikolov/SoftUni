2a26efae12d0c2d654761d55411d3c61592fe95c
Matey-Nikolov <matey.nikolov@mail.bg>
2023-04-08 18:26:21 +0300

    BankAccountsCustomers

---
+++ C#/Advance/Programming-Advanced-OOP/Defining Classes/Exercises/BankAccountsCustomers/BankAccount.cs
@@ -0,0 +1,39 @@
+﻿namespace BankAccountsCustomers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class BankAccount
+    {
+        public string AccountNumber { get; set; }
+        public string OwnerName { get; set; }
+        public decimal AccountBalance { get; set; }
+
+        public BankAccount(string accountNumber, string ownerName, decimal balance)
+        {
+            AccountNumber = accountNumber;
+            OwnerName = ownerName;
+            AccountBalance = balance;
+        }
+        public void MakeDeposit(decimal amount)
+        {
+            AccountBalance += amount;
+            Console.WriteLine($"Account balance: {AccountBalance}");
+        }
+
+        public void MakeWithdrawal(decimal amount)
+        {
+            if (amount < AccountBalance)
+            {
+                Console.WriteLine("Non-Sufficient Funds");
+            }
+            else
+            {
+                decimal fundsAvailable = AccountBalance - amount;
+                AccountBalance = fundsAvailable;
+                Console.WriteLine($"Withdrawn funds: {amount}. Funds available on the account: {fundsAvailable}");
+            }
+        }
+    }
+}
---
+++ C#/Advance/Programming-Advanced-OOP/Defining Classes/Exercises/BankAccountsCustomers/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace BankAccountsCustomers
+{
+    using System;
+    public class Program
+
+    {
+        static void Main()
+        {
+            BankAccount account = new BankAccount();
+
+
+        }
+    }
+}
