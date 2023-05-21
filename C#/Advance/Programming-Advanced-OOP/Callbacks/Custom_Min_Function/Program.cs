d1385163d3946ea7314188007a8e90ee7deecc44
Matey <80168510+Matey-Nikolov@users.noreply.github.com>
2023-05-21 14:48:41 +0300

    Custom_Min_Function

---
+++ C#/Advance/Programming-Advanced-OOP/Callbacks/Custom_Min_Function/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Custom_Min_Function
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int[], int> smallNumber = numberArr =>
+            {
+                int min = numberArr[0];
+                for (int i = 1; i < numbers.Length; i++)
+                {
+                    if (min > numberArr[i])
+                    {
+                        min = numberArr[i];
+                    }
+                }
+                return min;
+            };
+
+            Console.WriteLine(smallNumber(numbers));
+        }
+    }
+}
