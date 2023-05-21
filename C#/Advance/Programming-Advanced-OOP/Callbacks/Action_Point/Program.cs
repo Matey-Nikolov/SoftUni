1d751742d60dbff6d95c261182399fd6830d10c1
Matey <80168510+Matey-Nikolov@users.noreply.github.com>
2023-05-21 14:47:03 +0300

    Action_Point

---
+++ C#/Advance/Programming-Advanced-OOP/Callbacks/Action_Point/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Action_Point
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string[] names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Action<string> print = Console.WriteLine;
+            Array.ForEach(names, print);
+        }
+    }
+}
