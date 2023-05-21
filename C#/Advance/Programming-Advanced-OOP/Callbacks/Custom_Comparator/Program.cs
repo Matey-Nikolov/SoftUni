d898d1d68635719223b7521f69a8336620894877
Matey <80168510+Matey-Nikolov@users.noreply.github.com>
2023-05-21 14:47:55 +0300

    Custom_Comparator

---
+++ C#/Advance/Programming-Advanced-OOP/Callbacks/Custom_Comparator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace Custom_Comparator
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+
+            Func<int, int, int> sort = (numberOne, numberTwo) =>
+            {
+                if (numberOne % 2 != 0 && numberTwo % 2 == 0)
+                    return 1;
+                else if(numberOne % 2 == 0 && numberTwo % 2 != 0)
+                    return -1;
+
+                return numberOne.CompareTo(numberTwo);
+            };
+
+            Array.Sort(numbers, new Comparison<int>(sort));
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
