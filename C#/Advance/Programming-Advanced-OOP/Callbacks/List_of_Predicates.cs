52d87c959f8c21629ee2b6f7e320eb2e9f8cd089
Matey <80168510+Matey-Nikolov@users.noreply.github.com>
2023-05-22 18:41:42 +0300

    List_of_Predicates

---
+++ C#/Advance/Programming-Advanced-OOP/Callbacks/List_of_Predicates.cs
@@ -0,0 +1,36 @@
+﻿namespace List_of_Predicates
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int, int, bool> isDivisible = (i, d) => i % d == 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                bool divisible = true;
+
+                foreach (var number in input)
+                {
+                    if (!isDivisible(i, number))
+                    {
+                        divisible = false;
+                        break;
+                    }
+                }
+
+                if (divisible)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
