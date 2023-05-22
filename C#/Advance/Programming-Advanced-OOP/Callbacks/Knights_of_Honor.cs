befe79513e96b9dc16afd11254e4e73287f65834
Matey <80168510+Matey-Nikolov@users.noreply.github.com>
2023-05-22 18:40:44 +0300

    Add files via upload

---
+++ C#/Advance/Programming-Advanced-OOP/Callbacks/Knights_of_Honor.cs
@@ -0,0 +1,14 @@
+﻿namespace Knights_of_Honor
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string[] names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Action<string> print = name => Console.WriteLine($"Sir {name}");
+            Array.ForEach(names, print);
+        }
+    }
+}
