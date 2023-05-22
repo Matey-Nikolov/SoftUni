6c748c5e407736f8a26a58a86e50473e22ded758
Matey <80168510+Matey-Nikolov@users.noreply.github.com>
2023-05-22 18:40:21 +0300

    Add files via upload

---
+++ C#/Advance/Programming-Advanced-OOP/Callbacks/Find_Evens_or_Odds.cs
@@ -0,0 +1,39 @@
+﻿namespace Find_Evens_or_Odds
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            string command = Console.ReadLine();
+
+            int start = input[0];
+            int end = input[1];
+
+            Predicate<int> predicate = null;
+
+            switch (command)
+            {
+                case "odd":
+                    predicate = num => num % 2 != 0;
+                    break;
+                case "even":
+                    predicate = num => num % 2 == 0;
+                    break;
+            }
+
+            for (int i = start; i <= end; i++)
+            {
+                if (predicate(i))
+                {
+                    Console.Write(i + " ");
+                }
+            }
+
+        }
+    }
+}
