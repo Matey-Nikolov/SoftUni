0bc55a00d9610d6ce0234977c6dfd45a2f0895e5
Matey <80168510+Matey-Nikolov@users.noreply.github.com>
2023-05-24 16:42:21 +0300

    Predicate_for_Names

---
+++ C#/Advance/Programming-Advanced-OOP/Callbacks/Predicate_for_Names.cs
@@ -0,0 +1,25 @@
+﻿namespace Predicate_for_Names
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Func<string[], string[]> lengthInput = arr => arr.Where(x => x.Length <= n).ToArray();
+
+            Action<string[]> print = arr =>
+            {
+                foreach (var s in arr)
+                {
+                    Console.WriteLine(s);
+                }
+            };
+
+            print(lengthInput(input));
+        }
+    }
+}
