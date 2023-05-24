9a1fe78072c6f251d75f09116f01cb5ef19bcac6
Matey <80168510+Matey-Nikolov@users.noreply.github.com>
2023-05-24 16:43:21 +0300

    Reverse_and_Exclude

---
+++ C#/Advance/Programming-Advanced-OOP/Callbacks/Reverse_and_Exclude.cs
@@ -0,0 +1,26 @@
+﻿namespace Reverse_and_Exclude
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int[] inputNumbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int dividerN = int.Parse(Console.ReadLine());
+
+            Predicate<int> predicate = i => i % dividerN == 0;
+
+            Func<int[], Predicate<int>, int[]> remove = (num, predicate) => num.Where(i => !predicate(i)).ToArray();
+
+            Func<int[], int[]> reverse = numbers => numbers.Reverse().ToArray();
+
+            inputNumbers = remove(inputNumbers, predicate);
+            inputNumbers = reverse(inputNumbers);
+
+            Console.WriteLine(string.Join(" ", inputNumbers));
+        }
+    }
+}
