6bbca3e544e6c31b207f69e74cf1bc8b0b6dab4e
Matey <80168510+Matey-Nikolov@users.noreply.github.com>
2023-06-06 17:20:54 +0300

    TemplatePattern

---
+++ C#/Advance/Programming-Advanced-OOP/Design Patterns/Exercises/TemplatePattern/WholeWheat.cs
@@ -0,0 +1,15 @@
+﻿namespace TemplatePattern
+{
+    public class WholeWheat : Bread
+    {
+        public override void MixIngredients()
+        {
+            Console.WriteLine("Gathering Ingrediants for Whole Wheat Bread.");
+        }
+
+        public override void Bake()
+        {
+            Console.WriteLine("Baking the Whole Wheat Bread. (15 minutes)");
+        }
+    }
+}
