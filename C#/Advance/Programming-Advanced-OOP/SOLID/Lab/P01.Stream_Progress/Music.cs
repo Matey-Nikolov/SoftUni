9ee16a46cf20209f45614a20531ad6424fc732b7
Matey <80168510+Matey-Nikolov@users.noreply.github.com>
2023-06-19 17:47:08 +0300

    P01.Stream_Progress

---
+++ C#/Advance/Programming-Advanced-OOP/SOLID/Lab/P01.Stream_Progress/Music.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01.Stream_Progress
+{
+    public class Music : IStreamable
+    {
+        private string _artist;
+        private string _album;
+
+        public Music(string artist, string album, int length, int bytesSent)
+        {
+            this._artist = artist;
+            this._artist = album;
+            this.Length = length;
+            this.BytesSent = bytesSent;
+        }
+
+        public int Length { get; set; }
+
+        public int BytesSent { get; set; }
+    }
+}
---
+++ C#/Advance/Programming-Advanced-OOP/SOLID/Lab/P01.Stream_Progress/StreamProgressInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01.Stream_Progress
+{
+    public class StreamProgressInfo
+    {
+        //private File file;
+        private IStreamable _stream;
+
+        // If we want to stream a music file, we can't
+        public StreamProgressInfo(IStreamable file)
+        {
+            this._stream = file;
+        }
+
+        public int CalculateCurrentPercent()
+        {
+            return (this._stream.BytesSent * 100) / this._stream.Length;
+        }
+    }
+}
