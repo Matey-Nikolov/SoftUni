a693dc71402ec668fbaa2d07c640e950746f8f54
Matey <80168510+Matey-Nikolov@users.noreply.github.com>
2023-06-19 17:49:48 +0300

    P03.Detail_Printer

---
+++ C#/Advance/Programming-Advanced-OOP/SOLID/Lab/P03.Detail_Printer/Manager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03.DetailPrinter
+{
+    public class Manager : Employee
+    {
+        public Manager(string name, ICollection<string> documents) : base(name)
+        {
+            this.Documents = new List<string>(documents);
+        }
+
+        public IReadOnlyCollection<string> Documents { get; set; }
+
+
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(Name);
+            sb.Append(string.Join(Environment.NewLine, Documents));
+
+            return sb.ToString();
+        }
+    }
+}
