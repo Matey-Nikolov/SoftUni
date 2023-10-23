6ea731cc49d30efcb4ed63619bc46148d89658d8
Matey <matey.nikolov@mail.bg>
2023-10-23 18:14:45 +0300

    Sum_with_Task

---
+++ C#/Advance/Asynchronous Programming/Task/Lab/Sum_with_Task.cs
@@ -0,0 +1,27 @@
+﻿namespace Sum_with_Task
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Task<double> task = Task<double>.Run(() =>
+            {
+                return Computation(10);
+            });
+
+            Console.WriteLine($"{task.Result:f2}");
+        }
+
+        private static double Computation(double end)
+        {
+            double sum = 0;
+
+            for (int i = 0; i < end; i++)
+            {
+                sum += i * 1.1;
+            }
+
+            return sum;
+        }
+    }
+}
