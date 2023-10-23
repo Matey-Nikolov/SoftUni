e8f87a96ea5c3825751da58fd8c6550e1733fae8
Matey <matey.nikolov@mail.bg>
2023-10-23 18:15:05 +0300

    Long_Task

---
+++ C#/Advance/Asynchronous Programming/Task/Exercises/Long_Task.cs
@@ -0,0 +1,27 @@
+﻿namespace Long_Task
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Task task = new Task(Run);
+
+            task.Start();
+
+            //task.Wait();
+            
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("----------------");
+            }
+        }
+
+        static void Run()
+        {
+            while (true)
+            {
+                Console.WriteLine("Endless Loop...");
+            }
+        }
+    }
+}
