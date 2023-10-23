ec2bdcc424b9a9f8b9914147f15ad27b360d58a2
Matey <matey.nikolov@mail.bg>
2023-10-23 18:15:25 +0300

    Flower_Planting

---
+++ C#/Advance/Asynchronous Programming/Task/Exercises/Flower_Planting.cs
@@ -0,0 +1,35 @@
+﻿namespace Flower_Planting
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int difficultPlot = int.Parse(Console.ReadLine());
+
+            Task task = Task.Run(() =>
+            {
+                Seed(difficultPlot);
+            });
+
+            try
+            {
+                task.Wait();
+            }
+            catch (AggregateException ex)
+            { 
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void Seed(int plots)
+        {
+            for (int i = 0; i < 16; i++)
+            {
+                if (i == plots)
+                    throw new Exception("Something went wrong with seeding");
+
+                Console.WriteLine($"Seeding {i} plot.");
+            }
+        }
+    }
+}
