f9ddb3163d60a59fcf4a26f88ae8c2daf4eaf7d4
Matey <matey.nikolov@mail.bg>
2023-10-23 18:15:32 +0300

    Factorial

---
+++ C#/Advance/Asynchronous Programming/Task/Exercises/Factorial.cs
@@ -0,0 +1,31 @@
+﻿namespace Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            Task<int> sumFactorial = Task<int>.Run(() =>
+            {
+                return FindFactorial(number);
+            });
+
+            Console.WriteLine($"Factorial of {number} is: {sumFactorial.Result}");
+        }
+
+        static int FindFactorial(int x)
+        {
+            int sum = 1;
+
+            while (x != 1)
+            {
+                sum = sum * x;
+
+                x--;
+            }
+
+            return sum;
+        }
+    }
+}
