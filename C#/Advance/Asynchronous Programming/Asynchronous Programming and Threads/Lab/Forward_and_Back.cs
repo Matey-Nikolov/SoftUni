5aab8bd903065b2c77a5034ca55ba8837eb369c8
Matey <matey.nikolov@mail.bg>
2023-10-24 17:46:53 +0300

    Forward_and_Back

---
+++ C#/Advance/Asynchronous Programming/Asynchronous Programming and Threads/Lab/Forward_and_Back.cs
@@ -0,0 +1,34 @@
+﻿namespace Forward_and_Back
+{
+    public class Program
+    {
+        public static List<char> ListA = new List<char>() { 'o', 'o', 'x', 'o', 'o' };
+
+        static void Main()
+        {
+            Thread threadOne = new Thread(() => Forward());
+            threadOne.Start();
+
+            Thread threadTwo = new Thread(() => Back());
+            threadTwo.Start();
+
+            threadOne.Join();
+            threadTwo.Join();
+        }
+
+        private static void Forward()
+        {
+            Thread.Sleep(50);
+            ListA[4] = 'f';
+            Console.WriteLine(string.Join(' ', ListA));
+        }
+
+        private static void Back()
+        {
+            Thread.Sleep(100);
+            ListA[4] = 'o';
+            ListA[1] = 'b';
+            Console.WriteLine(string.Join(' ', ListA));
+        }
+    }
+}
