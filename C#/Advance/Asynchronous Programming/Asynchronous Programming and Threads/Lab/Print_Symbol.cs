04bfd9f77a99347c8c91a66ad1894ea00f28e0d8
Matey <matey.nikolov@mail.bg>
2023-10-24 17:46:44 +0300

    Print_Symbol

---
+++ C#/Advance/Asynchronous Programming/Asynchronous Programming and Threads/Lab/Print_Symbol.cs
@@ -0,0 +1,19 @@
+﻿namespace Print_Symbol
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Thread thread = new Thread(() => Go('O'));
+            thread.Start();
+
+            Go('x');
+        }
+
+        private static void Go(char symbol)
+        {
+            for (int i = 0; i < 450; i++)
+                Console.Write(symbol);
+        }
+    }
+}
