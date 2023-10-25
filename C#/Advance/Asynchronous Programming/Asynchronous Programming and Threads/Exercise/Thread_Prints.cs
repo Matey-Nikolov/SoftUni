d854aa8a496a58f51c4fcc23db716e7bba75b75a
Matey <matey.nikolov@mail.bg>
2023-10-25 18:31:14 +0300

    Thread_Prints

---
+++ C#/Advance/Asynchronous Programming/Asynchronous Programming and Threads/Exercise/Thread_Prints.cs
@@ -0,0 +1,35 @@
+﻿namespace Thread_Prints
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Thread threadNumber1 = new Thread(() => PrintNumbers0To10());
+            Thread threadNumber2 = new Thread(() => PrintNumbers11To20());
+
+            threadNumber1.Start();
+            threadNumber2.Start();
+
+            threadNumber2.Join();
+            threadNumber1.Join();
+        }
+
+        public static void PrintNumbers0To10()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Main thread: {i}");
+                Thread.Sleep(200);
+            }
+        }
+
+        public static void PrintNumbers11To20()
+        {
+            for (int i = 11; i < 20; i++)
+            {
+                Console.WriteLine($"Worker thread: {i}");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
