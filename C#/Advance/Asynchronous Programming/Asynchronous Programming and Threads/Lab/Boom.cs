825a53fd0fa55cd5eee20c27edbaf36797ea9dee
Matey <matey.nikolov@mail.bg>
2023-10-25 18:31:09 +0300

    Boom

---
+++ C#/Advance/Asynchronous Programming/Asynchronous Programming and Threads/Lab/Boom.cs
@@ -0,0 +1,35 @@
+﻿namespace Boom
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string inputText = Console.ReadLine();
+
+            Thread thread = new Thread(() => ContainsNumbers(inputText));
+            thread.Start();
+            thread.Join();
+        }
+
+        private static void ContainsNumbers(string inputText)
+        {
+            try
+            {
+                foreach (char item in inputText)
+                {
+                    if (char.IsDigit(item))
+                    {
+                        throw new Exception("Boom");
+                    }
+                }
+
+                Console.WriteLine("There are no bombs in the text.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
