c0645c82c824dada853a01951f9fce1617801f61
Matey <matey.nikolov@mail.bg>
2023-10-26 16:41:51 +0300

    Hot_And_Cold_2

---
+++ C#/Advance/Asynchronous Programming/Asynchronous Programming and Threads/Exercise/Hot_And_Cold.cs
@@ -0,0 +1,33 @@
+﻿namespace Hot_And_Cold
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Thread thread = new Thread(() => PrintCold());
+            thread.Start();
+
+            PrintHot();
+
+            thread.Join();
+        }
+
+        public static void PrintHot()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Hot");
+                Thread.Sleep(200);
+            }
+        }
+
+        public static void PrintCold()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Cold");
+                Thread.Sleep(200);
+            }
+        }
+    }
+}
---
+++ C#/Advance/Asynchronous Programming/Asynchronous Programming and Threads/Exercise/Hot_And_Cold_2.cs
@@ -0,0 +1,30 @@
+﻿namespace Hot_And_Cold_2
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Thread thread = new Thread(() => PrintCold());
+            thread.Start();
+
+            PrintHot();
+
+            thread.Join();
+        }
+
+        public static void PrintHot()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(200);
+                Console.WriteLine("Hot");
+            }
+        }
+
+        public static void PrintCold()
+        {
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine(" Cold ");
+        }
+    }
+}
