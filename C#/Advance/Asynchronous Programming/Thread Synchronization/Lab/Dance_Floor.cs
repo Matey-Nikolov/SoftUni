49a5ad9db86186ab61f5912588da6b7696cc37f8
Matey <matey.nikolov@mail.bg>
2023-11-23 17:24:35 +0200

    Dance_Floor.cs

---
+++ C#/Advance/Asynchronous Programming/Thread Synchronization/Lab/Dance_Floor.cs
@@ -0,0 +1,30 @@
+﻿namespace Dance_Floor
+{
+    public class Program
+    {
+        static Semaphore semaphore = new Semaphore(3, 3);
+
+        static void Main()
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                Thread thread = new Thread(TheDanceFloor);
+                thread.Name = $"{i}";
+                thread.Start(i);
+            }
+        }
+
+        public static void TheDanceFloor(object id)
+        {
+            semaphore.WaitOne();
+
+            Console.WriteLine($"Dancer number {Thread.CurrentThread.Name} entered.");
+            
+            Thread.Sleep(300 * (int)id);
+
+            Console.WriteLine($"Number: {Thread.CurrentThread.Name} left");
+
+            semaphore.Release();
+        }
+    }
+}
