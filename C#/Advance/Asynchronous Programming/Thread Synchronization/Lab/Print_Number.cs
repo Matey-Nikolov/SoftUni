ebcfc9cee4515bb369b015ddb9374863c3ae5355
Matey <matey.nikolov@mail.bg>
2023-11-23 17:24:05 +0200

    Print_Number.cs

---
+++ C#/Advance/Asynchronous Programming/Thread Synchronization/Lab/Print_Number.cs
@@ -0,0 +1,42 @@
+﻿namespace Print_Number
+{
+    public class Program
+    {
+        static readonly object locker = new object();
+
+        static void Main()
+        {
+            Thread threadPrint1 = new Thread(Print);
+            threadPrint1.Start();
+
+            Thread threadPrint2 = new Thread(Print);
+            threadPrint2.Start();
+        }
+
+        public static void Print()
+        {
+            Monitor.Enter(locker);
+
+            try
+            {
+                Console.WriteLine("Thread entered into the critical section.");
+
+                for (int i = 0; i < 2; i++)
+                {
+                    Console.WriteLine($"Number: {i}");
+                }
+            }
+            catch (SynchronizationLockException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            finally
+            {
+                Monitor.Exit(locker); // without this error
+                Console.WriteLine("Thread released.");
+            }
+
+        }
+    }
+}
