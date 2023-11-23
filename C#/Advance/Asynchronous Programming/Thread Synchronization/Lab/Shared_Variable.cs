2fc00fee3d7f0994049546a2c8f5a2b7e882d241
Matey <matey.nikolov@mail.bg>
2023-11-23 17:23:51 +0200

    Shared_Variable.cs

---
+++ C#/Advance/Asynchronous Programming/Thread Synchronization/Lab/Shared_Variable.cs
@@ -0,0 +1,39 @@
+﻿namespace Shared_Variable
+{
+    public class Program
+    {
+        private static int counter;
+        static readonly object locker = new object();
+
+        static void Main()
+        {
+            Thread threadPrintStars = new Thread(PrintStar);
+            threadPrintStars.Start();
+
+            Thread threadPrintPlus = new Thread(PrintPlus);
+            threadPrintPlus.Start();
+        }
+
+        static void PrintStar()
+        {
+            lock (locker)
+            {
+                for(counter = 0; counter < 5; counter++)
+                {
+                    Console.WriteLine(" * "); 
+                }
+            }
+        }
+
+        private static void PrintPlus()
+        {
+            lock (locker)
+            {
+                for (counter = 0; counter < 5; counter++)
+                {
+                    Console.WriteLine(" + ");
+                }
+            }
+        }
+    }
+}
