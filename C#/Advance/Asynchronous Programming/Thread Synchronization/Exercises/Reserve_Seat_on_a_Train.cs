a8e41b69f757a5d0022c603110cf8fc0387bda7d
Matey <matey.nikolov@mail.bg>
2023-12-04 17:10:01 +0200

    Reserve_Seat_on_a_Train

---
+++ C#/Advance/Asynchronous Programming/Thread Synchronization/Exercises/Reserve_Seat_on_a_Train.cs
@@ -0,0 +1,32 @@
+﻿namespace Reserve_Seat_on_a_Train
+{
+    public class Program
+    {
+        static bool reserve;
+        static object lockObject = new object();
+
+        static void Main()
+        {
+            Thread thread1 = new Thread(Reserve);
+            Thread thread2 = new Thread(Reserve);
+
+            thread1.Start();
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();
+        }
+
+        static void Reserve()
+        {
+            lock (lockObject)
+            {
+                if (!reserve)
+                {
+                    Console.WriteLine("Reserved");
+                    reserve = true;
+                }
+            }
+        }
+    }
+}
