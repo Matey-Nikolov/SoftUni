1f3ab79129bbb5dd5b9fd62f42f11b48dafdfa4b
Matey <matey.nikolov@mail.bg>
2023-12-04 17:10:34 +0200

    Bank

---
+++ C#/Advance/Asynchronous Programming/Thread Synchronization/Exercises/Bank/Account.cs
@@ -0,0 +1,48 @@
+﻿namespace Bank
+{
+    public class Account
+    {
+        private int Balance {  get; set; }
+        private readonly object balanceLock = new object();
+
+        public Account(int balance)
+        {
+            Balance = balance;
+        }
+
+        public int Withdraw(int amount)
+        {
+            lock (balanceLock)
+            {
+                try
+                {
+                    if (Balance < 0)
+                    {
+                        throw new ArgumentOutOfRangeException("Negative Balance");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred: {ex.Message}");
+                }
+
+                if (Balance >= amount)
+                {
+                    Console.WriteLine($"Balance before Withdrawal: {Balance}");
+                    Console.WriteLine($"Amount to Withdraw: -{amount}");
+
+                    Balance -= amount;
+
+                    Console.WriteLine($"Balance after Withdrawal: {Balance}");
+                }
+                else
+                {
+                    return 0;
+                }
+
+                return amount;
+            }
+
+        }
+    }
+}
---
+++ C#/Advance/Asynchronous Programming/Thread Synchronization/Exercises/Bank/Bank.cs
@@ -0,0 +1,30 @@
+﻿namespace Bank
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Thread[] threads = new Thread[10];
+
+            Account acc = new Account(500);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Thread t = new Thread(() =>
+                {
+                    acc.Withdraw(100);
+                });
+
+                threads[i] = t;
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                lock (threads[i])
+                {
+                    threads[i].Start();
+                }
+            }
+        }
+    }
+}
