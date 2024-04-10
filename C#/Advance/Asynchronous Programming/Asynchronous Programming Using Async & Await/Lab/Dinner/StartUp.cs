76206664b8a110e227949a5c0d166e6868c86c82
Matey <matey.nikolov@mail.bg>
2024-04-10 17:49:54 +0300

    Dinner

---
+++ C#/Advance/Asynchronous Programming/Asynchronous Programming Using Async & Await/Lab/Dinner/StartUp.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Dinner
+{
+    class StartUp
+    {
+        static async Task Main()
+        {
+            Task<Meat> meat = GrillMeatAsync();
+            Task<Vegetables> vegetables = GrillVegetablesAsync();
+
+            List<Task> dinnerTasks = new List<Task> { meat, vegetables };
+
+            await Task.WhenAll(dinnerTasks);
+
+            MakeFruitSalad();
+        }
+        private static async Task<Meat> GrillMeatAsync()
+        {
+           // Make the GrillMeatAsync() method work asynchronously
+            Console.WriteLine($"Putting the meat on the grill.");
+            await Task.Delay(3000);
+            Console.WriteLine($"Flipping the meat.");
+            await Task.Delay(3000);
+            Console.WriteLine("The meat is ready.");
+            return new Meat();
+        }
+        private static async Task<Vegetables> GrillVegetablesAsync()
+        {
+            // Make the GrillVegetablesAsync() method work asynchronously
+            Console.WriteLine($"Putting the vegetables on the grill.");
+            await Task.Delay(1500);
+            Console.WriteLine($"Flipping the vegetables.");
+            await Task.Delay(1500);
+            Console.WriteLine("The vegetables are ready.");
+            return new Vegetables();
+        }
+        private static FruitSalad MakeFruitSalad()
+        {
+            Console.WriteLine($"Fruit cutting.");
+            Thread.Sleep(500);
+            Console.WriteLine($"Mixing the fruit and adding yogurt.");
+            Thread.Sleep(100);
+            Console.WriteLine($"Crushing and adding nuts.");
+            Thread.Sleep(200);
+            Console.WriteLine($"Adding honey.");
+            Thread.Sleep(50);
+            return new FruitSalad();
+        }
+
+    }
+}
