f45bb6384d42d4404fcb72fb13ee9b009face679
Matey <matey.nikolov@mail.bg>
2024-04-10 17:49:04 +0300

    API_Content

---
+++ C#/Advance/Asynchronous Programming/Asynchronous Programming Using Async & Await/Lab/API_Content/Program.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace API_Content
+{
+    internal class Program
+    {
+        static async Task Main()
+        {
+            await GetAsync("https://retoolapi.dev/Icdxi0/data");
+        }
+
+        static async Task PostAsync(string uri)
+        {
+
+        }
+
+        static async Task GetAsync(string uri)
+        {
+            using (HttpClient client = new HttpClient())
+
+            using(var r = await client.GetAsync(new Uri(uri)))
+            {
+                string result = await r.Content.ReadAsStringAsync();
+
+                List<Customer> customers = JsonConvert.DeserializeObject<List<Customer>>(result);
+
+                foreach (var customer in customers)
+                {
+                    Console.WriteLine($"The customer named {customer.Name} has an id {customer.Id}");
+                }
+            }
+        }
+    }
+}
