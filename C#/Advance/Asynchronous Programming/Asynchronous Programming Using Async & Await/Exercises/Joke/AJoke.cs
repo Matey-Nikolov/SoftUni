22239a6896263d5de2eef21967b623a6c6445613
Matey <matey.nikolov@mail.bg>
2024-04-11 17:06:20 +0300

    Joke

---
+++ C#/Advance/Asynchronous Programming/Asynchronous Programming Using Async & Await/Exercises/Joke/AJoke.cs
@@ -0,0 +1,17 @@
+﻿namespace Joke
+{
+    public class AJoke
+    {
+        public int Id { get; set; }
+        public string Category { get; set; }
+        public string Type { get; set; }
+        public string Joke { get; set; }
+        public string Setup { get; set; }
+        public string Delivery { get; set; }
+
+        public AJoke()
+        {
+
+        }
+    }
+}
---
+++ C#/Advance/Asynchronous Programming/Asynchronous Programming Using Async & Await/Exercises/Joke/Program.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+
+namespace Joke
+{
+    public class Program
+    {
+        private static string uri = "https://v2.jokeapi.dev/joke/Any?safe-mode";
+        public static AJoke joke = new AJoke();
+
+        static async Task Main()
+        {
+            await GetAsync();
+        }
+
+        public static async Task GetAsync()
+        {
+
+            using (HttpClient client = new HttpClient())
+
+            using (var r = await client.GetAsync(new Uri(uri)))
+            {
+                string result = await r.Content.ReadAsStringAsync();
+
+                AJoke jsonObject = JsonConvert.DeserializeObject<AJoke>(result);
+
+                Console.WriteLine($"Joke id: {jsonObject.Id}");
+                Console.WriteLine($"Joke category: {jsonObject.Category}");
+
+                if (jsonObject.Type == "twopart")
+                {
+                    Console.WriteLine($"{jsonObject.Setup}");
+                    Console.WriteLine($"{jsonObject.Delivery}");
+                }
+                else
+                {
+                    Console.WriteLine($"{jsonObject.Joke}");
+                }
+
+            }
+
+
+
+        }
+    }
+}
