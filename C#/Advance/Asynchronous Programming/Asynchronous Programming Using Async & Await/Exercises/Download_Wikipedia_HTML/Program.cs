0e5788868b3a3d55c4ac3ec29bd4b576dac24212
Matey <matey.nikolov@mail.bg>
2024-04-11 17:05:58 +0300

    Download_Wikipedia_HTML

---
+++ C#/Advance/Asynchronous Programming/Asynchronous Programming Using Async & Await/Exercises/Download_Wikipedia_HTML/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Download_Wikipedia_HTML
+{
+    public class Program
+    {
+        private static string url = "https://developer.wikimedia.org";
+
+        static async Task Main()
+        {
+            await GetHTMLAsync();
+        }
+
+        public static async Task GetHTMLAsync()
+        {
+            using (HttpClient client = new HttpClient())
+
+            using (var request = await client.GetAsync(new Uri(url)))
+            {
+                var resultHTML = await request.Content.ReadAsStringAsync();
+
+                Console.WriteLine(resultHTML);
+            }
+        }
+    }
+}
