24bbda419709e16fd9791dce4cc8f00a62b80c25
Matey <matey.nikolov@mail.bg>
2024-04-16 16:41:49 +0300

    PermutationsRepetition

---
+++ C#/Advance/Algorithms/Combinatorial Algorithms/Exercises/PermutationsRepetition.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PermutationsRepetitions
+{
+    public class PermutationsRepetition
+    {
+        private static string[] permutations;
+
+        public static void Main()
+        {
+            permutations = Console.ReadLine()
+                .Split();
+            GeneratePermutations(0);
+        }
+
+        private static void GeneratePermutations(int index)
+        {
+            if (index >= permutations.Length)
+            {
+                Console.WriteLine(string.Join(" ", permutations));
+                return;
+            }
+
+            GeneratePermutations(index + 1);
+
+            HashSet<string> list = new HashSet<string> { permutations[index] };
+
+            for (int currentIndex = index + 1; currentIndex < permutations.Length; currentIndex++)
+            {
+                if (!list.Contains(permutations[currentIndex]))
+                {
+                    Swap(index, currentIndex);
+
+                    GeneratePermutations(index + 1);
+                    
+                    Swap(index, currentIndex);
+
+                    list.Add(permutations[currentIndex]);
+                }
+            }
+        }
+
+        private static void Swap(int first, int second)
+        {
+            (permutations[first], permutations[second]) = (permutations[second], permutations[first]);
+        }
+    }
+}
