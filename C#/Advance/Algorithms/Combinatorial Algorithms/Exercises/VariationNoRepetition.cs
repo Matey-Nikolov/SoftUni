de468e9fad000585c199e36205a949d295718807
Matey <matey.nikolov@mail.bg>
2024-04-18 09:31:49 +0300

    VariationNoRepetition

---
+++ C#/Advance/Algorithms/Combinatorial Algorithms/Exercises/VariationNoRepetition.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace VariationsNoRepetition
+{
+    public class VariationNoRepetition
+    {
+        private static int k;
+        private static string[] input;
+        private static string[] variations;
+        private static bool[] isUsed;
+
+        public static void Main()
+        {
+            input = Console.ReadLine()
+                .Split();
+            k = int.Parse(Console.ReadLine());
+            variations = new string[k];
+            isUsed = new bool[input.Length];
+
+            GenerateVariations(0);
+        }
+
+        private static void GenerateVariations(int depth)
+        {
+            if (depth >= variations.Length)
+            {
+                Console.WriteLine(string.Join(" ", variations));
+            }
+            else
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (!isUsed[i])
+                    {
+                        isUsed[i] = true;
+
+                        variations[depth] = input[i];
+                        GenerateVariations(depth + 1);
+
+                        isUsed[i] = false;
+                    }
+                }
+            }
+        }
+    }
+}
