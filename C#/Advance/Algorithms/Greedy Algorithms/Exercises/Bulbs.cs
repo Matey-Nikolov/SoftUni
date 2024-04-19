5f9f1d1df3742ecb7ea127be219f8526ebbbfc6c
Matey <matey.nikolov@mail.bg>
2024-04-19 11:34:56 +0300

    Bulbs

---
+++ C#/Advance/Algorithms/Greedy Algorithms/Exercises/Bulbs.cs
@@ -0,0 +1,30 @@
+﻿namespace Bulbs
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int[] bulbs = new int[] { 0, 1, 0, 1 };
+
+            int result = MinSwitches(bulbs);
+            Console.WriteLine(result);
+        }
+
+        public static int MinSwitches(int[] bulbs)
+        {
+            int count = 0;
+            int state = 0;
+
+            foreach (var bulb in bulbs)
+            {
+                if ((state % 2 == 0 && bulb == 0) || (state % 2 == 1 && bulb == 1))
+                {
+                    count++;
+                    state++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
