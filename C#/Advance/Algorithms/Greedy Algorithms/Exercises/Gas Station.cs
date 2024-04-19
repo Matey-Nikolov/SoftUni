04d0b993f1fd4c15682100ace136196f79b2b71b
Matey <matey.nikolov@mail.bg>
2024-04-19 11:35:13 +0300

    Gas Station

---
+++ C#/Advance/Algorithms/Greedy Algorithms/Exercises/Gas Station.cs
@@ -0,0 +1,28 @@
+﻿namespace Gas_Station
+{
+    public class Program
+    {
+        static void Main()
+        {
+
+        }
+
+        public static void GasStation()
+        {
+            double[] gas = new double[] { 1, 2 };
+            double[] distance = new double[] { 2, 1 };
+
+            Dictionary<int, int> proportions = new Dictionary<int, int>();
+
+            for (int i = 0; i < gas.Length; i++)
+            {
+                if (!proportions.ContainsKey(i))
+                {
+                    proportions[i] = 0;
+                }
+
+                //proportions[i] = gas[i] / distance[i];
+            }
+        }
+    }
+}
